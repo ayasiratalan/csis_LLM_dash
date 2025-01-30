ee48bf81bb8f858e38b13736a1def8a8889c3204
Abdullah yasir Atalan <76906943+ayasiratalan@users.noreply.github.com>
2025-01-30 15:33:00 -0500

    Update merged_dashboard.py

--- merged_dashboard.py
+++ merged_dashboard.py
@@ -56,7 +56,7 @@ def build_echarts_bar_option(x_data, series_data,
             "textStyle": {"fontSize": 12}
         },
         "legend": {
-            "bottom": 10,  # Moved legend to the bottom
+            "bottom": 0,  # Moved legend to the bottom
             "orient": "horizontal",
             "textStyle": {"fontSize": 12, "fontWeight": "bold"},
             "itemWidth": 20,
